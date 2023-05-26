1859e45498b46b56ab8fcdbd9605bfcf0a8cc84e
Barış Şahbaz <barissahbaz@Bars-iMac.local>
2023-05-26 22:51:02 +0300

    First Java for Android develop

---
+++ app/src/main/java/com/brsh/javalearning/First.java
@@ -0,0 +1,14 @@
+package com.brsh.javalearning;
+
+public class First {
+
+    public static void main(String[] args){
+        System.out.println("Hi");
+        System.out.println(5*2);
+
+
+
+
+    }
+
+}
